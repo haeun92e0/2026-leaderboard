133685a148920c77657981e760656a807040c624
haeun92e0 <gracekim8729@gmail.com>
2026-05-04 18:59:23 +0900

    Feat: 게임 결과 요청 DTO에 validation(@NotBlank) 추가

--- be/src/main/java/com/leaderboard/be/dto/ScoreSubmitRequest.java
+++ be/src/main/java/com/leaderboard/be/dto/ScoreSubmitRequest.java
@@ -1,12 +1,17 @@
 package com.leaderboard.be.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record ScoreSubmitRequest (
-
+    @NotBlank(message = "게임 이름은 필수입니다.")
     String gameName,
+
+    @NotBlank(message = "유저 식별 정보는 필수입니다.")
     String userId,
+
+    @NotBlank(message = "점수는 0 이상이어야 합니다.")
     double score
 
 ){}

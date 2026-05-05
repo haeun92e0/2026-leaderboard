399885abe0de16bf8b6ba6a0cca1644701efbadb
haeun92e0 <gracekim8729@gmail.com>
2026-05-05 20:48:19 +0900

    Feat: nickname에 unique 삭제

--- be/src/main/java/com/leaderboard/be/entity/User.java
+++ be/src/main/java/com/leaderboard/be/entity/User.java
@@ -19,7 +19,7 @@ public class User extends BaseEntity {
     @Column(name = "user_id", nullable = false, unique = true)
     private String userId;
 
-    @Column(name = "nickname", nullable = false, unique = true)
+    @Column(name = "nickname", nullable = false)
     private String nickname;
 
     @Column(name = "phone", unique = true)
